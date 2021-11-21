1fa498b1e1409ac8f792caffee7ca07f6d3cf99b
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-11-21 13:57:02 -0800

    2021_11_21
    Stack & Queue

---
+++ Java_Array/src/main/java/Queue.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Queue {
+
+    private static int MAX_QUEUE_SIZE = 10;
+    private int rear, front, size;
+    private int[] data = new int[MAX_QUEUE_SIZE];
+
+    public Queue() {
+        front = rear =size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int item) {
+        //환형 배열 구조
+        rear = (rear +1) % data.length;
+        data[rear] = item;
+        size ++;
+    }
+
+    public int pop() {
+        if(isEmpty()) throw new NoSuchElementException();
+        front = (front +1) % data.length;
+        int item = data[front];
+        data[front] = 0;
+        size --;
+
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty()) throw new NoSuchElementException();
+        return data[front];
+    }
+
+}
